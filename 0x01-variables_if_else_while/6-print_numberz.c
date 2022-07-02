0c8e5c03d9294a6f40f6665a6986b229d027bd66
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-01 21:14:24 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * main - single digit base 10 numbers
+ *
+ * Return: (0) for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (int i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+
+}
