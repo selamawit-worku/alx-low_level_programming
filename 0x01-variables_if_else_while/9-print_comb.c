ba6d871e76faf1b485a6141b42c7a93f9e759345
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-01 21:39:05 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: (0) for success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num =0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
