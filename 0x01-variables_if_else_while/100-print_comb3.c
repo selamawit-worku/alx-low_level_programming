2494ce156e694006fe9d55dd21c0a513ee07b79f
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-01 21:48:03 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <studio.h>
+
+/**
+ * main  - print combinations of two digit numbers
+ *
+ * Return: (0) for success
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if(tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return(0);
+}
