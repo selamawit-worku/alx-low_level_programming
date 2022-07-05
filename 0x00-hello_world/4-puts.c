b521a4fc110803ce849849505fc735aff02f22c4
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-04 17:29:10 -0700

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 - for success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
