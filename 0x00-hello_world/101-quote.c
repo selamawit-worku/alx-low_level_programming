5c294b257a9937fe14084c1274888cf4784c6860
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-04 17:59:48 -0700

    C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 1 for error
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
