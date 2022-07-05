319629808d708c49d8b31fc0e3417cb4f265f6fc
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-04 17:47:50 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,12 @@
 
 int main(void)
 {
+	long long longlong;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof('a'));
 	printf("Size of an int: %lu byte(s)\n", sizeof(0));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(1L));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(10000000000000000000000000L));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(0.1));
 	return (0);
 }
