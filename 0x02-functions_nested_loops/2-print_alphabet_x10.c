1364127ee85c249755c2cd24861535eadad46592
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-06 21:10:41 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(i);
+		}
+		putchar('\n');
+	}
+}
