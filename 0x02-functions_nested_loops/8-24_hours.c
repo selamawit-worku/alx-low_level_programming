e8d9ef49cd73806165b09063a7c991e4aa43ce87
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-06 22:06:44 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+	
+	for (h1 = 48; h1 <= 50; h1++)
+	{
+		for (h2 = 48; h2 <= 57; h2++)
+		{
+			for (m1 = 48; m1 <= 53; m1++)
+			{
+				for (m2 = 48; <= 57; m2++)
+				{
+					if (h1 >= 50 && h2 >= 52)
+						break;
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(58);
+					_putchar(m1);
+					_putchar(m2);
+				}
+			}
+		}
+	}
+}
