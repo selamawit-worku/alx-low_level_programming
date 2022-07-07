3ec79df31237cb0fd26bb90dfc600d155dac03cd
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-06 20:50:19 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,12 +11,11 @@ int main(void)
 	int i;
 	char p[] = "_putchar";
 
-	for(i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(p[i]);
 	}
 	_putchar('\n');
-	
-	return (0);
 
+	return (0);
 }
