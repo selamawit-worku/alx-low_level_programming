f9f2f7d11824dd29576e3f9eabb7557d7471f4e9
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-06 20:57:26 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet 
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(i);
+
+}
