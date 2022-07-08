926ce259cd189d402ece2b298201312d650930ce
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-07 20:31:29 -0700

    C more loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,12 @@ void more_numbers(void)
 	char j;
 	
 	for (i = 0; i < 10; i++)
-	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 				_putchar ((j / 10) + '0');
 			_putchar((j % 10) + '0');
+			if (j == 14)
+				_putchar('\n');
 		}
-		_putchar('\n');
-	}
 }
