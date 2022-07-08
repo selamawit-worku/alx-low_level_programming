f4db914b201bfb3c688e5223c3fec83738fd8acd
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-07 20:08:18 -0700

    C more loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ */
+int main(void)
+{
+	long i, n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
