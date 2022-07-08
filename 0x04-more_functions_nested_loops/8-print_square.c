b0e6caaf97aeaff6a27f4a6b3c5573e1a6f5e5f9
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-07 20:38:50 -0700

    C more loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,11 +15,14 @@ void print_square(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
+		{
 			_putchar('#');
-
+		}
 		_putchar('\n');
 	}
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 }
