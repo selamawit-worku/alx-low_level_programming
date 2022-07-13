5ea9c70e1348e0fee1fd54969b6a688313112d0e
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-12 20:10:11 -0700

    Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,17 @@ int _atoi(char *s)
 {
 	int res = 0;
 	int i;
+	char sign = '+';
 
-	for (i = 0; s[i] != '\0'; ++i)
-		res = res * 10 + s[i] - '0';
+	for (i = 0; s[i] != '\0'; ++i){
+		if ((s[i] >= 48 && s[i] <= 57))
+		{
+			if(i != 0 && s[i - 1] == '-')
+				sign = '-';
+			res = res * 10 + s[i] - '0';
+		}
+	}
+	if (sign == '-')
+		res *= -1;
 	return res;
 }
