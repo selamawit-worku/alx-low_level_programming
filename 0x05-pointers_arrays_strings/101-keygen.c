9481e97a99432f2b9dada5c2005c571e275527b9
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-12 21:43:43 -0700

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
+
+	srand(time(NULL));
+	while(s != 2772)
+	{
+		i = k = l =0;
+
+		while ((2772 - 122) > l)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			l += c[j];
+			i++;
+		}
+		while(c[k])
+		{
+			if (c[k] == (2772 - l))
+			{
+				p[i] = c[k];
+				l += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
+	}
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
+}
