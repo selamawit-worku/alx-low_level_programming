858fe58f8e2da6ecf5200de948eb8a4463901882
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-14 19:37:39 -0700

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
-	for (; j< n; j++)
+	for (; j < n; j++)
 		dest[j] = '\0';
 	return (dest);
 }
