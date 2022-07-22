b3881d0213167519096a1296b7c5664f299361d9
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-21 19:31:07 -0700

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * recursion_sqrt - return sqrt
+ * @s: start 
+ * @e: end
+ * @n: num
+ * Return: int
+ */
+
+int  recursion_sqrt(int s, int e, int n)
+{
+	int m;
+
+	if (e >= s)
+	{
+		m = s + (e - s) / 2;
+		if (m * m == n)
+			return (m);
+		if (m * m > n)
+			retrun (recursion_sqrt(s, m - 1, n));
+		if (m * m < n)
+			return (recursion_sqrt(m + 1, e, n));
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - sqrt of a number
+ * @n: number
+ * Return: square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+	       return (n);
+	return (recursion_sqrt(2, n, n));
+}
