cf917ac8c02a9c4ec503612c13e0cfd140bfd8c0
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-21 18:42:30 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion(char *s)
+ * @s: string ptr
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return (len);
+	_strlen_recursion(s + len++);
+}
