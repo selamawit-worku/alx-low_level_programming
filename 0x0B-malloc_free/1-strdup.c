b4d8de520018a8aeccdb34ed42f196db1262f8d5
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-25 17:58:39 -0700

    C - malloc, free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - return pointer to mem loca
+ * @str: string 
+ * Return: pointer to string
+ */
+
+char *_strdup(char *str)
+{
+	char *strdup;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	strdup = malloc(sizeof(*str) * ++i);
+	if (strdup == NULL)
+		return (NULL);
+	for (j = 0; str[j] != '\0'; j++)
+		strdup[j] = str[j];
+	return (strdup);
+}
