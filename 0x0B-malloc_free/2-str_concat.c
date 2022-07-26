4ec209a32f3d1456411ff90939a2feacacabe08b
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-25 18:30:30 -0700

    C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,14 +14,14 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 
 	if (s1 == NULL)
-		return s2;
+		s1 = "";
 	if (s2 == NULL)
-		return s1;
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	str = malloc(sizeof(char) * (i + j +1));
+	str = malloc(sizeof(char) * (i + j + 1));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
