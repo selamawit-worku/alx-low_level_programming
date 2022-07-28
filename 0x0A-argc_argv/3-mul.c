3c43bc4dd68b1ee6227d206d227f9d8e25db06f7
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-27 18:15:58 -0700

    C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-	int result;
-
 	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	result = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", result);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
-	
 }
