a0c1bce17aa0a3156fcfe68f076a03601ca822af
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-28 18:40:03 -0700

    C - more malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; s1[i] != '\0';i++)
 		result[i] = s1[i];
-	for(j = 0; j < n && s2[j] != '\0'; j++, i++)
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
 		result[i] = s2[j];
 	result[i] = '\0';
 	return (result);
