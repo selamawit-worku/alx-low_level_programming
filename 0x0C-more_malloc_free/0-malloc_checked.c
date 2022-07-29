295012a89c3e888274177b76d01afd296d234380
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-28 18:18:59 -0700

    C - more malloc, free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: unsigned int
- * Return: void *
+ * Return: pointer to memory allocated memory
  */
 
 void *malloc_checked(unsigned int b)
