006aa89c0d08da3ec9fdcae1721740a7befbf08c
selamawit-worku <selamawit.endalew@gmail.com>
2022-07-28 18:07:11 -0700

    C - more malloc, free

---
+++ 0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ * Return: void * 
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return(p);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - main function
+ *@c: char
+ * Return: 0 for success
+ */
+
+int _putchar(char c)
+{
+	return (0);
+}
